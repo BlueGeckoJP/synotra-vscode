f2725d5c91bcf2404976fe3a350b6887b84152ff
BlueGeckoJP <kei09doya@gmail.com>
2025-11-23 04:30:53 +0900

    feat: implement completion item provider in completion.ts and update activation in extension.ts

---
+++ src/completion.ts
@@ -0,0 +1,14 @@
+import type * as vscode from "vscode";
+
+export default class Completion implements vscode.CompletionItemProvider {
+	public provideCompletionItems(
+		document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken,
+		context: vscode.CompletionContext,
+	): vscode.ProviderResult<
+		vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>
+	> {
+		return [];
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import type * as vscode from "vscode";
+import * as vscode from "vscode";
+import Completion from "./completion";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log("synotra-vscode is now active! YATTAZE!");
+
+	const completionProvider = vscode.languages.registerCompletionItemProvider(
+		"synotra",
+		new Completion(),
+	);
+	context.subscriptions.push(completionProvider);
 }
 
 // This method is called when your extension is deactivated
